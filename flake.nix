35729191f9dc6f764e9416412874c6448355605e
vkhashimoto <git@knj.email>
2026-05-26 23:29:25 -0300

    feat(flake): add package derivation

--- flake.nix
+++ flake.nix
@@ -16,15 +16,30 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        dcd = pkgs.stdenv.mkDerivation {
+          pname = "dcd";
+          version = "0.1.0";
+          src = ./.;
+          nativeBuildInputs = [ pkgs.zig ];
+          dontConfigure = true;
+          buildPhase = ''
+            zig build install \
+              --prefix $out \
+              -Doptimize=ReleaseSafe \
+              --global-cache-dir $(mktemp -d)
+          '';
+          dontInstall = true;
+        };
       in
       {
+        packages.default = dcd;
+
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.nixfmt
             pkgs.zig
             pkgs.zls
           ];
-
         };
       }
     );
