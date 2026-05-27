b1a43eb8ad6399606469eca4a1c522b19bbd9c9e
vkhashimoto <git@knj.email>
2026-05-26 22:36:09 -0300

    chore: add flake.nix and .gitignore

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A Dynamic Change Directory (dcd)";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nixfmt
+            pkgs.zig
+            pkgs.zls
+          ];
+
+        };
+      }
+    );
+}
